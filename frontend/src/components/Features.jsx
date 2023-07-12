05de89654f9a1f1f5c216b023a2399369504ea33
Ashish Jose <ashishjose2675@gmail.com>
2023-07-12 12:23:05 +0530

    styles: features section

--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -1,28 +1,33 @@
+import {
+  BookmarkSquareIcon,
+  CalendarIcon,
+  QueueListIcon,
+} from '@heroicons/react/24/outline';
 import { ArrowTrendingUpIcon } from '@heroicons/react/24/solid';
 
 const data = [
   {
-    bgColor: '',
+    bgColor: 'bg-purple-600',
     title: 'Progress tracking and Certifications',
     icon: <ArrowTrendingUpIcon className='h-6 w-6 text-white' />,
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugit delectus accusamus',
   },
   {
-    bgColor: '',
+    bgColor: 'bg-yellow-300',
     title: 'Diverse course selection',
-    icon: <ArrowTrendingUpIcon className='h-6 w-6 text-white' />,
+    icon: <QueueListIcon className='h-6 w-6 text-white' />,
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugit delectus accusamus',
   },
   {
-    bgColor: '',
+    bgColor: 'bg-indigo-600',
     title: 'Accessibility and Convinience',
-    icon: <ArrowTrendingUpIcon className='h-6 w-6 text-white' />,
+    icon: <CalendarIcon className='h-6 w-6 text-white' />,
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugit delectus accusamus',
   },
   {
-    bgColor: '',
+    bgColor: 'bg-rose-600',
     title: 'Interative learning experience',
-    icon: <ArrowTrendingUpIcon className='h-6 w-6 text-white' />,
+    icon: <BookmarkSquareIcon className='h-6 w-6 text-white' />,
     desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam fugit delectus accusamus',
   },
 ];
@@ -40,9 +45,9 @@ const Features = () => {
         {data.map((d, i) => (
           <div
             key={i}
-            className='bg-white/30 backdrop-blur-md shadow-sm rounded-md p-4 px-6 border border-gray-100'
+            className='bg-white rounded-md p-4 px-6 border border-gray-200'
           >
-            <div className='bg-purple-600 w-fit p-3 rounded-md shadow-lg'>
+            <div className={`w-fit p-3 rounded-md shadow-lg ${d.bgColor} `}>
               {d.icon}
             </div>
             <h2 className='text-xl mt-4 mb-2'>{d.title}</h2>
